50b46c3d682f8f595d1ac8a0fed357da240580ef
Molly <qc8659ba@go.minneapolis.edu>
2019-04-11 18:39:29 -0500

    error  connecting

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+import  HelloAPI from '@/services/HelloAPI'
+
+Vue.config.productionTip = false;
+
+Vue.prototype.$hello_api = HelloAPI;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
